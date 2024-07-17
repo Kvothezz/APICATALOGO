7e853b160596a153205d5c7737d7f668e22a6a0c
Fábio Bezerra <bezerradeoliveirafabio@gmail.com>
2024-07-17 18:27:29 -0300

    Data Annotations e migrations para popular tabelas

--- APICatalogo/Migrations/20240717112252_AjusteTabelas.Designer.cs
+++ APICatalogo/Migrations/20240717112252_AjusteTabelas.Designer.cs
@@ -4,6 +4,7 @@ using APICatalogo.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace APICatalogo.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240717112252_AjusteTabelas")]
+    partial class AjusteTabelas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -78,7 +81,7 @@ namespace APICatalogo.Migrations
                         .HasColumnType("varchar(80)");
 
                     b.Property<decimal>("Preco")
-                        .HasColumnType("decimal(10,2)");
+                        .HasColumnType("decimal(10,2");
 
                     b.HasKey("Id");
 
--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
-
+[Table("Categorias")]
 public class Categoria
 {   
     public Categoria() { // boas práticas: inicializar a coleção
-       Produtos = new Collection<Produto>();  
+       Produtos = new Collection<Produto>();
     }
+    [Key]
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
 
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
 
 
